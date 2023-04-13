65dc563a093f4c63f5099f2bebf133c1f8396ec5
kimgome0222 <130648386+kimgome0222@users.noreply.github.com>
2023-04-13 19:12:58 +0900

    Add files via upload

--- java/ch04/WhileStudy2.java
+++ java/ch04/WhileStudy2.java
@@ -1,15 +1,18 @@
-//package com.green.java.ch04;
-//
-//public class WhileStudy2 {
-//    public static void main(String[] args) {
-//    // do-while문
-//
-//        do {
-//
-//        }while(false);
-//
-//
-//        }
-//    }
-//
-//}
+package com.green.java.ch04;
+
+public class WhileStudy2 {
+    public static void main(String[] args) {
+        //do-while문
+
+        do {
+
+        } while(false);
+
+
+        while(true) {
+
+        }
+
+
+    }
+}
